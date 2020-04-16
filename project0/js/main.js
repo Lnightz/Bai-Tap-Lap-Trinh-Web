6eab266e936708dcc5b12e1ba090c9160fe2ee8e
Vuong Hieu <lightzzero@gmail.com>
2020-04-17 00:56:19 +0700

    add Silder

---
+++ project0/js/main.js
@@ -0,0 +1,10 @@
+/** First Slider */
+$(".slider-one")
+    .not(".slick-intialized")
+    .slick({
+        autoplay:true,
+        autoplaySpeed: 3000,
+        prevArrow :".site-slider .slider-btn .prev",
+        nextArrow :".site-slider .slider-btn .next",
+        dots: true,
+    });
